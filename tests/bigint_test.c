436cbf762662065ec607cad57337b28122ae2657
s2ks <bakkerjilles@gmail.com>
2022-06-08 22:05:00 +0200

    add multiplication tests

--- tests/bigint_test.c
+++ tests/bigint_test.c
@@ -1,19 +1,65 @@
 #include <stdio.h>
-/*#include <sys/random.h>*/
-
-/*#include <gperftools/profiler.h>*/
+#include <string.h>
+#include <assert.h>
 
 #define __DEBUG_OUT stdout
 
 #include "bigint.h"
 
+static const BIGINT *testres[] = {
+	(BIGINT[64]) {0x00, 0x91, 0xc3, 0x47, 0x2d, 0x36},
+	(BIGINT[64]) {0x64},
+	(BIGINT[64]) {0x00, 0xe1, 0xf5, 0x05},
+};
+
+void test_mul(void);
+
+static const void (*test[])(void) = {
+	test_mul,
+};
+
+void test_mul(void) {
+	BIGINT a[64] = {0x10, 0xee, 0xab};
+	BIGINT b[64] = {0x10, 0xab, 0x50};
+
+	bigint_mul(a, b, sizeof(a));
+
+	assert(memcmp(a, testres[0], sizeof(a)) == 0);
+
+	memset(a, 0, sizeof(a));
+	a[0] = 10;
+
+	bigint_mul(a, a, sizeof(a));
+
+	assert(memcmp(a, testres[1], sizeof(a)) == 0);
+
+	memset(a, 0, sizeof(a));
+	a[0] = 0x10;
+	a[1] = 0x27;
+
+	bigint_mul(a, a, sizeof(a));
+
+	_bigint_dbgprint(a, sizeof(a));
+
+	assert(memcmp(a, testres[2], sizeof(a)) == 0);
+}
+
 int main(void) {
 
 	/* TODO this should be the usage */
-	BIGINT a[64];
-	bigint_set(a, "25e100", sizeof(a));
+	/*BIGINT a[64];*/
+	/*bigint_set(a, "1e150", sizeof(a));*/
 
-	bigint_print(a, sizeof(a)); /* --> prints a 1 with 150 zeros */
+	/*bigint_print(a, sizeof(a)); [> --> prints a 1 with 150 zeros <]*/
+	/*_bigint_dbgprint(a, sizeof(a));*/
+
+
+	test_mul();
+
+	BIGINT a[64] = {10};
+
+	bigint_pow(a, 20, sizeof(a));
+
 	_bigint_dbgprint(a, sizeof(a));
 
 	return 0;
